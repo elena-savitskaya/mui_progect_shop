c0069ab226a924a7dc358133756d80034bc6b789
ElenaS35 <helena0518savitskaya@gmail.com>
2022-12-30 12:38:13 +0200

    new

---
+++ src/components/Basket.jsx
@@ -0,0 +1,53 @@
+import { Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import { ShoppingBasket } from "@mui/icons-material";
+import BasketItem from "./BasketItem";
+
+const Basket = (props) => {
+    const {
+        cartOpen,
+        closeCart = Function.prototype,
+        order = [],
+        removeFromOrder
+    } = props;
+
+    return (
+        <Drawer
+            anchor="right"
+            open={cartOpen}
+            onClose={closeCart}
+        >
+            <List sx={{width: '400px'}}>
+                <ListItem>
+                    <ListItemIcon>
+                        <ShoppingBasket />
+                    </ListItemIcon>
+                    <ListItemText primary="Cart" />
+                </ListItem>
+                <Divider />
+
+                {!order.length ? (
+                    <ListItem>Cart is empty!</ListItem>
+                ) : (
+                    <>
+                    {order.map((item) => (
+                        <BasketItem key={item.name} removeFromOrder={removeFromOrder} {...item} />
+                    ))}
+                    <Divider />
+                    <ListItem>
+                        <Typography sx={{fontWeight: 700}}>
+                            Total price:{' '}
+                            {order.reduce((acc, item) => {
+                            return acc + item.price * item.quantity;
+                            }, 0)}{' '}
+                            $.
+                        </Typography>
+                    </ListItem>
+                    </>
+                )}
+
+            </List>
+        </Drawer>
+    )
+}
+
+export default Basket
---
+++ src/components/Search.jsx
@@ -0,0 +1,18 @@
+import { TextField } from "@mui/material";
+
+const Search = (props) => {
+    const { onChange, value } = props;
+
+    return <TextField
+        sx={{
+            mb: '1.5rem'
+        }}
+        label="search"
+        variant="standard" 
+        fullWidth
+        type='search'
+        value={value}
+        onChange={onChange} />;
+};
+
+export default Search;
